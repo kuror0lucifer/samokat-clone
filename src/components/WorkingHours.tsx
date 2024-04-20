fdf2484231cad33b0ef44d7566658d41aea04c04
kuror0lucifer <kovalishin.2014@mail.ru>
2024-04-20 23:56:11 +0300

    added working time component depending on the user's current time

---
+++ src/components/WorkingHours.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { IoIosMoon } from 'react-icons/io';
+
+const WorkingHours: React.FC = () => {
+  return (
+    <div className='workingHours'>
+      <IoIosMoon className='workingHours__icon' />
+      <span className='workingHours__text'>Работаем с 08:00 до 23:00</span>
+    </div>
+  );
+};
+
+export default WorkingHours;
