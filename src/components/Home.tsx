c920915592f9d147abf3767eaff71531a630f68e
kuror0lucifer <kovalishin.2014@mail.ru>
2024-04-21 00:42:37 +0300

    fixed bug

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,9 +3,9 @@ import Categories from './Categories';
 import WorkingHours from './WorkingHours';
 
 const currentHours: Date = new Date();
-const offHours: number[] = [23, 24, 1, 2, 3, 4, 5, 6, 7];
+const offHours: number[] = [23, 0, 1, 2, 3, 4, 5, 6, 7];
 
-const Home = () => {
+const Home: React.FC = () => {
   return (
     <section className='section'>
       <Categories />
