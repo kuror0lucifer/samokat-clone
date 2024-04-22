4b7e900a1f0ff030da86ddb0001b9552292e7cbb
kuror0lucifer <kovalishin.2014@mail.ru>
2024-04-22 19:51:18 +0300

    added address confirm to aside right

--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
 
 const Location: React.FC = () => {
-  return <aside className='location'></aside>;
+  return (
+    <aside className='location'>
+      <div className='sidebar'>
+        <div className='addressConfirm'>
+          <h3 className='addressConfirm__title'>Ваш город Москва?</h3>
+          <p className='addressConfirm__text'>
+            Цены и акции могут отличаться в зависимости от расположения
+          </p>
+          <div className='buttons'>
+            <button className='confirm-button'>Верно</button>
+            <button className='confirm-button'>Выбрать другой</button>
+          </div>
+        </div>
+      </div>
+    </aside>
+  );
 };
 
 export default Location;
