399e35d3a404549ea616681cfeeb056d07c1df79
kuror0lucifer <kovalishin.2014@mail.ru>
2024-04-23 16:17:16 +0300

    added a map to the right aside

--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -4,15 +4,18 @@ const Location: React.FC = () => {
   return (
     <aside className='location'>
       <div className='sidebar'>
-        <div className='addressConfirm'>
-          <h3 className='addressConfirm__title'>Ваш город Москва?</h3>
-          <p className='addressConfirm__text'>
+        <div className='address-confirm'>
+          <h3 className='address-confirm__title'>Ваш город Москва?</h3>
+          <p className='address-confirm__text'>
             Цены и акции могут отличаться в зависимости от расположения
           </p>
           <div className='buttons'>
             <button className='confirm-button'>Верно</button>
             <button className='confirm-button'>Выбрать другой</button>
           </div>
+        </div>
+        <div className='map__container'>
+          <div className='map'></div>
         </div>
       </div>
     </aside>
