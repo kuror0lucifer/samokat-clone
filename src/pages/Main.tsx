812e506704672d08d2ad58622ce6a4324497a1e2
kuror0lucifer <kovalishin.2014@mail.ru>
2024-04-25 20:34:08 +0300

    updated styles for main

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const Main: React.FC = () => {
-  return <main className='main'></main>;
+  return (
+    <main className='main'>
+      <div className='main__container'>
+        <div>
+          <span className='main__container-text'>Доставка</span>&nbsp;
+          <span className='main__container-text'>от 15 минут</span>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Main;
