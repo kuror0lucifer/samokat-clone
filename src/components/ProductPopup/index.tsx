e1b1f44a7e34979af84ad7007492ac62262d31f4
kuror0lucifer <kovalishin.2014@mail.ru>
2024-09-24 18:49:50 +0200

    animation popup added

--- src/components/ProductPopup/index.tsx
+++ src/components/ProductPopup/index.tsx
@@ -21,26 +21,50 @@ export const ProductPopup: React.FC<ProductPopupProps> = ({
   isPopupVisible,
   setIsPopupVisible,
 }) => {
-  if (!productInfo) return null;
+  const [isMounted, setIsMounted] = React.useState(false); // Контроль DOM
+  const [isAnimating, setIsAnimating] = React.useState(false); // Контроль анимации
+
+  // Управляем состоянием анимации и монтированием
+  React.useEffect(() => {
+    if (isPopupVisible) {
+      setIsMounted(true); // Вставляем в DOM
+      setTimeout(() => setIsAnimating(true), 10); // Небольшая задержка перед началом анимации открытия
+    } else if (isMounted) {
+      setIsAnimating(false);
+      setTimeout(() => setIsMounted(false), 500);
+    }
+  }, [isPopupVisible, isMounted]);
+
+  if (!isMounted) return null;
 
   return (
     isPopupVisible && (
-      <nav className={styles.drawer__container}>
-        <DrawerHeader setIsPopupVisible={setIsPopupVisible} />
-        <div className={styles.root}>
-          <div className={styles.container}>
-            <ImgHeader productInfo={productInfo} />
-            <div className={styles.content}>
-              <PopupTitle productInfo={productInfo} />
-              <ShareButton />
-              <Description productInfo={productInfo} />
-              <Nutritions productInfo={productInfo} />
-              <Attributes productInfo={productInfo} />
-              <PriceButton productInfo={productInfo} />
+      <div className={styles.drawer}>
+        <div
+          className={styles.overlay}
+          onClick={() => setIsPopupVisible(false)}
+        ></div>
+        <nav
+          className={`${styles.drawer__container} ${
+            isAnimating ? styles.visible : styles.hidden // Добавляем класс hidden для закрытия
+          }`}
+        >
+          <DrawerHeader setIsPopupVisible={setIsPopupVisible} />
+          <div className={styles.root}>
+            <div className={styles.container}>
+              <ImgHeader productInfo={productInfo} />
+              <div className={styles.content}>
+                <PopupTitle productInfo={productInfo} />
+                <ShareButton />
+                <Description productInfo={productInfo} />
+                <Nutritions productInfo={productInfo} />
+                <Attributes productInfo={productInfo} />
+                <PriceButton productInfo={productInfo} />
+              </div>
             </div>
           </div>
-        </div>
-      </nav>
+        </nav>
+      </div>
     )
   );
 };
